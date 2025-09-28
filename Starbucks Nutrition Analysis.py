6102963b0b21bf14ce8ad855d7a6b19c35382a1f
Yusif Hasanli <yusifhasanlien@gmail.com>
2025-09-28 18:01:32 +0400

    Add files via upload

---
+++ Starbucks Nutrition Analysis.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np  
+import matplotlib.pyplot as plt      
+import seaborn as sns    
+
+data_drink = pd.read_csv("C:\Users\tomas\Desktop\starbucks_data\starbucks-menu-nutrition-drinks.csv", index = 0)
+print(data_drink)
